d2480dee60109ed8b2c9d1b48ba41a113646d123
Feliperp1640 <feliperp1640@gmail.com>
2026-06-13 17:26:12 -0500

    agrego tipo de datos

--- tipos.py
+++ tipos.py
@@ -8,3 +8,4 @@ print(f"El valor es {entero} y su tipo es {type(entero)}")
 print(f"El valor es {decimal} y su tipo es {type(decimal)}")
 print(f"El valor es {verdadero} y su tipo es {type(verdadero)}")
 print(f"El valor es {falso} y su tipo es {type(falso)}")
+
