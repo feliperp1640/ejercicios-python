b74d97c2f1b6f73173f8776a56ed62203b29fc66
Feliperp1640 <feliperp1640@gmail.com>
2026-06-13 11:47:56 -0500

    agrego tipos de datos

---
+++ tipos.py
@@ -0,0 +1,10 @@
+texto = "esto es un string"
+entero = 42
+decimal = 3.14
+verdadero = True
+falso = False
+print(f"El valor es {texto} y su tipo es {type(texto)}")
+print(f"El valor es {entero} y su tipo es {type(entero)}")
+print(f"El valor es {decimal} y su tipo es {type(decimal)}")
+print(f"El valor es {verdadero} y su tipo es {type(verdadero)}")
+print(f"El valor es {falso} y su tipo es {type(falso)}")
