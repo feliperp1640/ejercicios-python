d997bab7995bd65135a2c687d5b9ad845b6f1ead
Feliperp1640 <feliperp1640@gmail.com>
2026-06-13 11:27:02 -0500

    agregue la variable experiencia con su print correspondiente

--- saludo.py
+++ saludo.py
@@ -1,3 +1,5 @@
 ciudad= "villavicencio"
 profesion="backend developer"
+experiencia=0
 print(f"Soy de {ciudad} y quiero ser {profesion}")
+print(f"tengo {experiencia} anos de experiencia en {profesion}")
