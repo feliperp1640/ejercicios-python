9dfaea434683091a5b0d99d100ccf6bbbbb24ed3
Feliperp1640 <feliperp1640@gmail.com>
2026-06-13 17:24:05 -0500

    segunda presentacion

--- presentacion.py
+++ presentacion.py
@@ -2,5 +2,4 @@ nombre="Felipe"
 ciudad="villavicencio"
 edad= 32
 objetivo= "backend developer"
-
 print(f"Me llamo {nombre} soy de la ciudad de {ciudad} tengo {edad} anos y quiero estudiar {objetivo}")
