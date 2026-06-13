9a5b8e0b0ba06539d79cd7f3477dfd7bf4905e53
Feliperp1640 <feliperp1640@gmail.com>
2026-06-13 11:35:40 -0500

    mi primera presntacion

---
+++ presentacion.py
@@ -0,0 +1,6 @@
+nombre="Felipe"
+ciudad="villavicencio"
+edad= 32
+objetivo= "backend developer"
+
+print(f"Me llamo {nombre} soy de la ciudad de {ciudad} tengo {edad} anos y quiero estudiar {objetivo}")
