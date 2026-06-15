9ada62aeaae63e6c85cf9c6de9b0f9f941621a45
Feliperp1640 <feliperp1640@gmail.com>
2026-06-15 13:49:56 -0500

    Ejercicio de notas

---
+++ nota.py
@@ -0,0 +1,9 @@
+nota= 45
+if nota >= 90:
+  print("Excelente")
+elif nota >=70 and nota <=89:
+     print("Aprobado")
+elif nota >=60 and nota <=69:
+     print("Aprobado rasante")
+else:
+    print("Reprobado")
