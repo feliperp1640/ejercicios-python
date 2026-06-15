6008c13bee1cc819c071a71c37543c93a78bb703
Feliperp1640 <feliperp1640@gmail.com>
2026-06-15 15:43:16 -0500

    mi primer lista utilizando for

--- precios.py
+++ precios.py
@@ -1,6 +1,6 @@
-listas=[15000, 32000, 8000, 47000]
+precios=[15000, 32000, 8000, 47000]
 
-for lista in listas:
+for lista in precios:
     if lista < 20000:
         print("Precio accesible")
     else:
