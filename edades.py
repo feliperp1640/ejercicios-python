c7478b8fe7a4ad598d7357ffb07d24fe3e1c477e
Feliperp1640 <feliperp1640@gmail.com>
2026-06-15 17:41:27 -0500

    actualizo lista con for anadiendole un contador

--- edades.py
+++ edades.py
@@ -1,6 +1,11 @@
 edad=[15,23,17,30,14,25,16]
 
+#for edades in edad:
+    #if edades >= 18:
+     # print(f"{edades} es mayor de edad")
+
+contador = 0
 for edades in edad:
     if edades >= 18:
-      print(f"{edades} es mayor de edad")
-   
+        contador = contador + 1
+print(f"Hay {contador} mayores de edad")
