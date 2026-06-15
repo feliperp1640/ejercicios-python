a75179386ffdd629ae6cb1cedf73d4adfbb0e696
Feliperp1640 <feliperp1640@gmail.com>
2026-06-15 15:20:14 -0500

    actualizacion de la lista

--- listas.py
+++ listas.py
@@ -2,3 +2,6 @@ usuarios = ["felipe", "maria", "carlos", "ana"]
 
 for usuario in usuarios:
     print (f"Hola,{usuario}")
+
+usuarios.append("luis")#agregar un elemento a la lista
+print(len(usuarios)) #cuantos elementos tiene
