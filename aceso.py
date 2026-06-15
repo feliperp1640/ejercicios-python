2b8d75bc60b01117033fa1c954b47edbae70a211
Feliperp1640 <feliperp1640@gmail.com>
2026-06-15 09:56:02 -0500

    actualizacion con elif

--- aceso.py
+++ aceso.py
@@ -1,7 +1,8 @@
 usuario= "felipe"
 contrasena= 1234
-
 if usuario == "felipe" and contrasena == 1234:
     print("Acceso permitido")
+elif usuario == "felipe" and contrasena != 1234:
+    print("Contrasena incorrecta")    
 else:
-    print("Acceso denegado")    
+    print("Usuario no encontrado")        
