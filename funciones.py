1b139332f292df0a419aac71bdb2cefead0fcea6
Feliperp1640 <feliperp1640@gmail.com>
2026-06-16 18:37:28 -0500

    Mi primera funcion

---
+++ funciones.py
@@ -0,0 +1,9 @@
+def saludar(nombre):
+    mensaje = f"Hola, {nombre}. Bienvenido al sistema."
+    return mensaje
+
+resultado = saludar("felipe")
+print(resultado)
+
+resultado2 = saludar("maria")
+print(resultado2)
