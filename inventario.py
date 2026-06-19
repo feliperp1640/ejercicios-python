130bb50fcf65897f7d06809b5e719647ffd62baa
Feliperp1640 <feliperp1640@gmail.com>
2026-06-18 21:09:01 -0500

    diccionario recorrido con for

---
+++ inventario.py
@@ -0,0 +1,8 @@
+productos = [
+    {"nombre": "mouse", "precio": 25000},
+    {"nombre": "teclado", "precio": 45000},
+    {"nombre": "monitor", "precio": 350000}
+]
+for producto in productos:
+     print(f"El producto {producto['nombre']} cuesta {producto['precio']}")
+
