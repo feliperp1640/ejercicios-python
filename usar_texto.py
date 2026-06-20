ba5e6b1c2c90aac733b7247f4517e0ab712cd0e2
Feliperp1640 <feliperp1640@gmail.com>
2026-06-20 10:58:04 -0500

    llamar import texto

---
+++ usar_texto.py
@@ -0,0 +1,7 @@
+from texto_utils import a_mayusculas
+texto = a_mayusculas("Felipe")
+print(texto)
+
+from texto_utils import contar_palabras
+texto1 = contar_palabras("Felipe es backend developer")
+print(texto1)
