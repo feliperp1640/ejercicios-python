b6c58ba76e9b606f0847801baaf2e42a770c2741
Feliperp1640 <feliperp1640@gmail.com>
2026-06-22 21:33:48 -0500

    clase usuario

---
+++ clases.py
@@ -0,0 +1,11 @@
+class Usuario:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        print(f"Hola, soy {self.nombre} y tengo {self.edad} anos")
+
+felipe = Usuario("felipe", 32)
+felipe.saludar()
+
